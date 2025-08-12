30d4bec071934b962fd84a1a88e79b35b4d2b890
Yeiner99SD <yeinermoreno2005@gmail.com>
2025-08-11 22:06:21 -0500

    login funcional

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,10 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+    devToolbar:{ 
+        enabled: false
+    },
     vite:{
        plugins: [tailwindcss()],
-    }
+    },
 });
