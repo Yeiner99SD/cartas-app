ccb8fa6fa517e6980cd50fe74fe16dbcb0652df6
Yeiner99SD <yeinermoreno2005@gmail.com>
2025-08-17 12:45:55 -0500

    Musica

---
+++ src/data/cartas.ts
@@ -0,0 +1,27 @@
+export const cartas = [
+  {
+    title: "Gongoló",
+    image: "/gongolibg.webp",
+    slug: "gongoli",
+    from: "Tu hombre",
+    to: "Gongoló",
+    text: "Sembré algodón y coseché una nube.",
+  },
+  {
+    title: "Marla Singer",
+    slug: "marla-singer",
+    image: "/gongolibg.webp",
+    from: "Tu hombre",
+    to: "Marla",
+    text: "En cada carta hay un pedacito de mí esperándote.",
+  },
+  {
+    title: "Dulce carita",
+    image: "/gongolibg.webp",
+    slug: "dulce-carita",
+    from: "Tu hombre",
+    to: "Dulce",
+    text: "Tus sonrisas son la tinta con la que escribo mi felicidad.",
+  },
+  
+];
