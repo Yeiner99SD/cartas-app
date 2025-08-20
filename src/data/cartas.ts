1aa7e1136c33f98af615608b3849277cb31d9da8
Yeiner99SD <yeinermoreno2005@gmail.com>
2025-08-19 22:11:18 -0500

    Avance totall del dash y de la primer carta "gongolí"

--- src/data/cartas.ts
+++ src/data/cartas.ts
@@ -1,11 +1,28 @@
-export const cartas = [
+interface Carta {
+  title: string;
+  image: string;
+  slug: string;
+  from: string;
+  to: string;
+  text: string;
+  components: string[];
+}
+
+export const cartas: Carta[] = [
   {
-    title: "Gongoló",
+    title: "Gongoli",
     image: "/gongolibg.webp",
     slug: "gongoli",
     from: "Tu hombre",
     to: "Gongoló",
     text: "Sembré algodón y coseché una nube.",
+    components: [
+      'HeroGongoli',
+      'Content1',
+      'Content2',
+      'Content3',
+      'ReflectionGongoli'
+    ]
   },
   {
     title: "Marla Singer",
@@ -14,6 +31,11 @@ export const cartas = [
     from: "Tu hombre",
     to: "Marla",
     text: "En cada carta hay un pedacito de mí esperándote.",
+    components: [
+      'HeroMarla',
+      'ContentMarla1',
+      'ContentMarla2'
+    ]
   },
   {
     title: "Dulce carita",
@@ -22,6 +44,10 @@ export const cartas = [
     from: "Tu hombre",
     to: "Dulce",
     text: "Tus sonrisas son la tinta con la que escribo mi felicidad.",
-  },
-  
+    components: [
+      'HeroDulce',
+      'ContentDulce1',
+      'ContentDulce2'
+    ]
+  }
 ];
