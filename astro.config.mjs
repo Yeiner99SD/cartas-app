25c43eebd3027a0cf19344618c5ef7f03fcba014
Yeiner99SD <yeinermoreno2005@gmail.com>
2026-07-28 23:49:03 -0500

    "add vercel adapter for SSR page"

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel';
+
 
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+    adapter: vercel(),
   devToolbar: { 
       enabled: false
   },
